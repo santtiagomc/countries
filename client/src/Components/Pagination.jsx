3abee7bf26bf29168e843d9b8b68b2d2aa38d74c
Santiago Moreno <santtiagomc@gmail.com>
2022-10-12 20:33:43 -0500

    pagination cambio de primero a ultimo

--- client/src/Components/Pagination.jsx
+++ client/src/Components/Pagination.jsx
@@ -6,6 +6,38 @@ export default function Pagination({cityPerPage, allCitys, paginado, currentPage
     const [input, setInput] = useState(currentPage)
     const totalPages = Math.ceil(allCitys / cityPerPage)
 
+    const [pageDisplayed, /*setPageDisplayed*/] = useState(4);
+    const [maxPageDisplayed, setMaxPageDisplayed] = useState(4);
+    const [minPageDisplayed, setMinPageDisplayed] = useState(0);
+
+    const movePagesPrevius = (page) => {
+        if (page === 1) movePages(0)
+        else if (page <= 2 && page > 0) {
+            setMaxPageDisplayed(4);
+            setMinPageDisplayed(0);
+        } else {
+            if (page === 0) page = 1;
+            let maxi = page + pageDisplayed - 3;
+            let mini = maxi - pageDisplayed;
+            setMaxPageDisplayed(maxi);
+            setMinPageDisplayed(mini);
+        }
+    }
+
+    const movePages = (page) => {
+        if (page === pageNumber?.length) {
+            movePagesPrevius(0)
+            setMaxPageDisplayed(4);
+            setMinPageDisplayed(0);
+        } else {
+            if (page === 0) page = pageNumber?.length;
+            let maxi = page + pageDisplayed - 1;
+            let mini = maxi - pageDisplayed;
+            setMaxPageDisplayed(maxi);
+            setMinPageDisplayed(mini);
+        }
+    }
+
     const pageRange = (currentPage, totalPages) => {
         let start = currentPage - 2;
         let end = currentPage + 2;
@@ -23,6 +55,17 @@ export default function Pagination({cityPerPage, allCitys, paginado, currentPage
             end: end,
         }
     }
+
+    const firstPage = (e) => {
+        movePages(1)
+        paginado(1)
+    }
+
+    const lastPage = (e) => {
+        movePages( totalPages - 1)
+        paginado(totalPages)
+    }
+
     
     function nextPage() {
         setCurrentPage(currentPage + 1);
@@ -42,6 +85,7 @@ export default function Pagination({cityPerPage, allCitys, paginado, currentPage
     return (
         <nav>
             <ul className={style.lista1}>
+            <button onClick={(e) => firstPage(e)}> ... </button>
             <button onClick={prevPage} className={style.btn} disabled={currentPage === 1 || currentPage < 1}>Prev</button>
             {pageNumber?.map(number => (
                     <li key={number}>
@@ -51,6 +95,7 @@ export default function Pagination({cityPerPage, allCitys, paginado, currentPage
                     </li>
                 ))}
                 <button onClick={nextPage} className={style.btn} disabled={currentPage === Math.ceil(totalPages) || currentPage > Math.ceil(totalPages)}>Next</button>
+                <button onClick={(e) => lastPage(e)}> ... </button>
             </ul>
         </nav>
     )
